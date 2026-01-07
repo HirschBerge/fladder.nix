6072715aa7d20b8eb8db85f57e10bc95b1fbb79b
DestinyofYeet <ole@ole.blue>
2026-01-07 12:26:43 +0100

    chore(version): bump version

--- pkg.nix
+++ pkg.nix
@@ -3,20 +3,19 @@
 let
   cleanName = "Fladder";
 
-  version = "0.8.0";
-  versionCommit = "a00b5255992ca4b6d5ba5eb5513e960ed780c198";
+  version = "0.9.0";
 
   src = fetchurl {
     url =
       "https://github.com/DonutWare/Fladder/releases/download/v${version}/Fladder-Linux-${version}.AppImage";
-    hash = "sha256-uU/QTfAF22d4RPkv7rJrOkn1/LBwMJRv0wLnolxDD/Y=";
+    hash = "sha256-L9dyqEGrMlGW6C7Jj4nhM5X/DlJ3vDNL4pSlsVel8Iw=";
   };
 
   ghSource = fetchFromGitHub {
     owner = "DonutWare";
     repo = "Fladder";
-    rev = versionCommit;
-    hash = "sha256-Rpnf4fYsChbCsezBtmqQ8xkaj6HmfnDPvZLSZjPEPJ0=";
+    tag = "v${version}";
+    hash = "sha256-IX3qbIgfi9d8rP24yIGlBzi5l28YQWnvLD+dD+7uIZc=";
   };
 
   pname = "fladder";
