52623b78c7aa5eb28a492ee409762c96080544cf
DestinyofYeet <ole@ole.blue>
2026-02-12 22:41:07 +0100

    chore: cleaned up flake.nix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 This flake packages [Fladder](https://github.com/DonutWare/Fladder/).
 
-Unfortunately, using `flutter.buildFlutterApplication` fails, because Fladder uses MediaKit, which doesn't build in the Nix build env.
+Unfortunately, using `flutter.buildFlutterApplication` fails, because Fladder uses MediaKit, which doesn't build in the Nix build env (see `test` branch).
 That's why the appimage was used.
--- flake.nix
+++ flake.nix
@@ -1,16 +1,18 @@
 {
   description = "A very basic flake";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs, }@inputs:
-    let pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in {
+  outputs =
+    { self, nixpkgs }@inputs:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
       devShells.x86_64-linux.default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [ ];
-
-        # uncomment this is you get some kind of ssl error, usually on anything networking related using reqwest
-        # PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
       };
 
       packages.x86_64-linux.default = pkgs.callPackage ./pkg.nix { };
