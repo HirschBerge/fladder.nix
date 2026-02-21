c8c421068588183e1a5f29492938be51eccf8efe
HirschBerge <hirschy.kirkwood@proton.me>
2026-02-20 21:54:30 -0500

    Updated to v0.10.0

--- pkg.nix
+++ pkg.nix
@@ -1,21 +1,24 @@
-{ pkgs, appimageTools, fetchurl, stdenv, fetchFromGitHub }:
-
-let
+{
+  pkgs,
+  appimageTools,
+  fetchurl,
+  stdenv,
+  fetchFromGitHub,
+}: let
   cleanName = "Fladder";
 
-  version = "0.9.0";
+  version = "0.10.0";
 
   src = fetchurl {
-    url =
-      "https://github.com/DonutWare/Fladder/releases/download/v${version}/Fladder-Linux-${version}.AppImage";
-    hash = "sha256-L9dyqEGrMlGW6C7Jj4nhM5X/DlJ3vDNL4pSlsVel8Iw=";
+    url = "https://github.com/DonutWare/Fladder/releases/download/v${version}/Fladder-Linux-${version}.AppImage";
+    hash = "sha256-LU2IFM7Jt/Cbr3BNMgUrNFER0RD/oTlBoG1a+znsC1M=";
   };
 
   ghSource = fetchFromGitHub {
     owner = "DonutWare";
     repo = "Fladder";
     tag = "v${version}";
-    hash = "sha256-IX3qbIgfi9d8rP24yIGlBzi5l28YQWnvLD+dD+7uIZc=";
+    hash = "sha256-wY2h6lJWFH7UNxzGrSOLDj4cjAY9VAG8IUYaxWugVQQ=";
   };
 
   pname = "fladder";
@@ -23,7 +26,7 @@ let
   build = appimageTools.wrapType2 {
     inherit pname version src;
 
-    extraPkgs = (_: with pkgs; [ mpv libepoxy ]);
+    extraPkgs = _: with pkgs; [mpv libepoxy];
   };
 
   desktopEntry = pkgs.makeDesktopItem {
@@ -33,17 +36,17 @@ let
     exec = "${build}/bin/${pname} %f";
     icon = "${ghSource}/icons/production/fladder_icon_desktop.png";
   };
+in
+  stdenv.mkDerivation {
+    name = pname;
 
-in stdenv.mkDerivation {
-  name = pname;
+    src = build;
 
-  src = build;
+    installPhase = ''
+      mkdir -p $out/bin
+      cp bin/${pname} $out/bin/${pname}
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp bin/${pname} $out/bin/${pname}
-
-    mkdir -p $out/share/applications
-    cp ${desktopEntry}/share/applications/${pname}.desktop $out/share/applications/${pname}.desktop
-  '';
-}
+      mkdir -p $out/share/applications
+      cp ${desktopEntry}/share/applications/${pname}.desktop $out/share/applications/${pname}.desktop
+    '';
+  }
